432d8f01d88a4ca6cf679e7047ee00c1c2ecff76
Rajan Gautam <gautamrajan073@gmail.com>
2021-07-26 12:35:06 +0530

    Backend - Main App Added

---
+++ backend/real_estate/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/',
+         TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/realtors/', include('realtors.urls')),
+    path('api/contacts/', include('contacts.urls')),
+    path('api/contactSeller/', include('contactSeller.urls')),
+    path('api/listings/', include('listings.urls')),
+    path('api/orders/', include('order.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
+]
